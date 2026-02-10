bd3988edc0970749c3f45563aa4adb978c05c6a9
IG27 <154378488+gibby2k@users.noreply.github.com>
2026-02-10 12:23:41 +0100

    Update Secrets.h

--- SapMain/Secrets.h
+++ SapMain/Secrets.h
@@ -5,8 +5,8 @@
 // #define WIFI_SSID "Orange_Swiatlowod_2710"
 // #define WIFI_PASSWORD "yUTFjgs4PqpHxx5KhG"
 
-#define WIFI_SSID "iPhone (Igor)"
-#define WIFI_PASSWORD "dzepsiu123"
+#define WIFI_SSID "wifissid"
+#define WIFI_PASSWORD "haslo"
 
 #define MQTT_SERVER "broker.hivemq.com"
 #define MQTT_PORT 1883
@@ -14,5 +14,6 @@
 // Unikalny temat
 #define MQTT_TOPIC_STATUS  "pbs/student/sap/status"
 #define MQTT_TOPIC_CONTROL "pbs/student/sap/control"
+
 
 #endif
